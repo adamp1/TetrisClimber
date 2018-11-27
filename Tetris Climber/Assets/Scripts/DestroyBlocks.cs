aa73b7156e0ddc75222412a4240eec1447e7746e
TornadoFranz <45045911+TornadoFranz@users.noreply.github.com>
2018-11-27 17:49:55 +0100

    Energiebalken

--- Tetris Climber/Assets/Scripts/DestroyBlocks.cs
+++ Tetris Climber/Assets/Scripts/DestroyBlocks.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DestroyBlocks : MonoBehaviour
 {
+    public Image Energy;
+    float energy;
+    public float EnergyRecovery;
+    public float SliceCost;
+
     bool lookRight = false;
     bool lookLeft = true;
     bool inisiateSlice;
+    bool sliceEnergy;
     float moveSwordy = 1;
     float moveSwordx = 0;
-    float movesSworddown = 1;
+    float movesSworddown = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +27,18 @@ public class DestroyBlocks : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Energy
+        energy = Energy.fillAmount;
+        energy *= 100;
+
+        //Energy Recovery
+        if(energy < 100)
+        {
+            energy += EnergyRecovery * Time.deltaTime;
+        }
+
+
+        //Slice
         Vector3 pos = transform.position;
 
         if (Input.GetAxis("Horizontal") > 0)
@@ -35,13 +54,17 @@ public class DestroyBlocks : MonoBehaviour
 
         if (Input.GetButtonDown("Slice"))
         {
-            Debug.Log("Slice");
-            inisiateSlice = true;
+            if(energy >= SliceCost)
+            {
+                inisiateSlice = true;
+                sliceEnergy = true;
+            }
         }
 
 
         if(inisiateSlice)
         {
+            //Raycast
             RaycastHit hit;
             Ray Slice = new Ray(new Vector3(pos.x, pos.y + movesSworddown, pos.z), new Vector3(moveSwordx, moveSwordy, 0));
             Debug.DrawRay(new Vector3(pos.x, pos.y + movesSworddown, pos.z), new Vector3(moveSwordx, moveSwordy, 0));
@@ -74,7 +97,7 @@ public class DestroyBlocks : MonoBehaviour
             }
 
 
-            if (Physics.Raycast(Slice, out hit, 0.8f))
+            if (Physics.Raycast(Slice, out hit, 0.75f))
             {
                 if (hit.collider.gameObject.tag == "Mino")
                 {
@@ -82,7 +105,29 @@ public class DestroyBlocks : MonoBehaviour
                 }
 
             }
+
+            if(sliceEnergy)
+            {
+                //Energieverbrauch
+                if(energy > SliceCost)
+                {
+                    energy -= SliceCost;
+                }
+                   
+                sliceEnergy = false;
+            }
+
         }
 
+        //Show Energy
+        energy /= 100;
+        Energy.fillAmount = energy;
+
     }
+
+    //BUGS: Blöcke übern spieler gehen noch nicht kaputt
+    //      L Reverse geht nicht kaputt
+    //      Lücken zwischen Blöcken
+    //      Blöcken fallen noch nicht nachdem ein Block zerstört wurdea
+    //      Blöcke spawnen unendlich auf einer stelle falls spieler stehen bleibt
 }
