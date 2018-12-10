6097e7f2590214440b2d2018d6b89fc4b6882ebe
adamp1 <45352374+adamp1@users.noreply.github.com>
2018-12-10 12:40:54 +0100

    movetex start

---
+++ Tetris Climber/Assets/Scripts/MoveTexture.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTexture : MonoBehaviour
+{
+    public float speed = 1;
+    public Vector2 direction = new Vector2(1, 1);
+    public string map = "_EmissionMap";
+
+    Material mat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
