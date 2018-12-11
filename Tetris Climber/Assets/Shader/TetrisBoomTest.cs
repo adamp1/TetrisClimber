f80477d24a92f99038ea1f3ddb94faaee96b64da
adamp1 <45352374+adamp1@users.noreply.github.com>
2018-12-11 17:12:25 +0100

    boom test

---
+++ Tetris Climber/Assets/Shader/TetrisBoomTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisBoomTest : MonoBehaviour
+{
+    public float speed = 8;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    IEnumerator boom(){
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
