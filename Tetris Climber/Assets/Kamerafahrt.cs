3b473d2d6be97394b42a1cc1ae468d0e5469cc84
adamp1 <45352374+adamp1@users.noreply.github.com>
2018-12-13 14:59:19 +0100

    title screen

---
+++ Tetris Climber/Assets/Kamerafahrt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kamerafahrt : MonoBehaviour
+{
+
+    public Transform target;
+    Vector3 startpos;
+    public float speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(startpos, target.position, Mathf.Pow(Mathf.Sin(Time.time * speed), 2));
+    }
+}
