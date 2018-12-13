6974763508944552d12a4c6cff2786d122db0a3f
adamp1 <45352374+adamp1@users.noreply.github.com>
2018-12-13 11:29:21 +0100

    danger problems

---
+++ Tetris Climber/Assets/Scripts/BlendshapePlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlendshapePlayer : MonoBehaviour
+{
+    SkinnedMeshRenderer skinned;
+
+    public float blend0, blend1;
+    public float speed = 1;
+    public float max = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        skinned = GetComponent<SkinnedMeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float gleichung = Mathf.Abs(Mathf.Sin(Time.time * speed)) * max;
+        float gleichung2 = Mathf.Abs(Mathf.Cos(Time.time * speed)) * max;
+        blend0 = gleichung;
+        blend1 = gleichung2;
+        
+
+        skinned.SetBlendShapeWeight(0, blend0);
+        skinned.SetBlendShapeWeight(1, blend1);
+    }
+}
