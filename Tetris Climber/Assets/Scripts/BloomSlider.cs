b62d37522979963882453b700237bf89feea3801
adamp1 <45352374+adamp1@users.noreply.github.com>
2019-01-02 11:35:32 +0100

    bloom slider

--- Tetris Climber/Assets/Scripts/BloomSlider.cs
+++ Tetris Climber/Assets/Scripts/BloomSlider.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+using UnityEngine.UI;
 
 public class BloomSlider : MonoBehaviour
 {
+
+    public float bloom = 0;
+    public float bloommin, bloommax;
+    Bloom effect;
+    Slider slider;
+    
     // Start is called before the first frame update
     void Start()
     {
+
+        slider = GetComponent<Slider>();
+        effect = GameObject.FindObjectOfType<Bloom>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        effect.bloomIntensity = Mathf.Lerp(bloommin, bloommax, slider.value);
     }
 }
