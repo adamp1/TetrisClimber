62e376676b0c9996dfee99798d8d56a65fab5102
adamp1 <45352374+adamp1@users.noreply.github.com>
2019-01-03 14:01:22 +0100

    screenshake reagiert auf danger

--- Tetris Climber/Assets/Scripts/ScreenshakeHelper.cs
+++ Tetris Climber/Assets/Scripts/ScreenshakeHelper.cs
@@ -9,9 +9,10 @@ public class ScreenshakeHelper : ImageEffectBase
 {
     public float minshake = 0.004f;
     public float maxshake = 0.01f;
-    public float shake;
+    public float shake = 0;
     public bool noshake = true;
     Game game;
+    float dangerdistance;
     void Awake()
     {
         game = GameObject.FindObjectOfType<Game>();
@@ -19,7 +20,18 @@ public class ScreenshakeHelper : ImageEffectBase
     // Called by camera to apply image effect
     void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        shake = Mathf.Lerp(minshake, maxshake, 0);
+        dangerdistance = game.distancetodanger;
+
+
+
+        shake = Mathf.Lerp(minshake, maxshake, 1-(dangerdistance/3));
+
+        if (dangerdistance < 3){
+            noshake = false;
+        }
+        else{
+            noshake = true;
+        }
 
         if (!noshake)
         {
