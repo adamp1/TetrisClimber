bbf790cc8fe93e021220ba302a9efc1914779c83
adamp1 <45352374+adamp1@users.noreply.github.com>
2019-01-28 15:42:55 +0100

    blinking ui in progress

---
+++ Tetris Climber/Assets/PressAnyButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PressAnyButton : MonoBehaviour
+{
+    public GameObject menu;
+    public AnimationCurve curve;
+    public float speed = 0.4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey){
+            menu.SetActive(true);
+            gameObject.SetActive(false);
+        }
+
+
+    }
+}
