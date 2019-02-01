d9f316efd20ee9b9cee78b91f2ba459b8e85b5e7
adamp1 <45352374+adamp1@users.noreply.github.com>
2019-02-01 11:07:10 +0100

    game over ui wieder sichtbar

---
+++ Tetris Climber/Assets/UISpace.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISpace : MonoBehaviour
+{
+    Game game;
+    Canvas canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        game = GameObject.FindObjectOfType<Game>();
+        canvas = GetComponent<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (game.GameOverUI.active || game.GameOver2UI.active){
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        }
+    }
+}
