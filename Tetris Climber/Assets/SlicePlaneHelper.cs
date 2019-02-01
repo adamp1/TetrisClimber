9c29e3993fd8591b1ce60018d8a67f627ddac75f
adamp1 <45352374+adamp1@users.noreply.github.com>
2019-02-01 15:38:49 +0100

    sliceplanehelper "works"

--- Tetris Climber/Assets/SlicePlaneHelper.cs
+++ Tetris Climber/Assets/SlicePlaneHelper.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class SlicePlaneHelper : MonoBehaviour
 {
+
+    DestroyBlocks spieler;
+    MeshRenderer renderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spieler = GameObject.FindObjectOfType<DestroyBlocks>();
+        renderer = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (spieler.inisiateSlice){
+            renderer.enabled = true;
+        }
+        else{
+            renderer.enabled = false;
+        }
     }
 }
