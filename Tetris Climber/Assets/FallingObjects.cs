0c87b0e0bfb0f485b629344933157855b9375484
adamp1 <45352374+adamp1@users.noreply.github.com>
2019-02-05 13:12:05 +0100

    storyeinblendung start

--- Tetris Climber/Assets/FallingObjects.cs
+++ Tetris Climber/Assets/FallingObjects.cs
@@ -6,6 +6,9 @@ public class FallingObjects : MonoBehaviour
 {
     public GameObject[] objects;
     public float min, max;
+    public float spawndistance = 50;
+    public float speed = 5;
+    public float deathdistance = 100;
     // Start is called before the first frame update
     void Start()
     {
---
+++ Tetris Climber/Assets/Scripts/StoryEinblendung.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryEinblendung : MonoBehaviour
+{
+
+    public Game game;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StoryStart(){
+        //ganz viel zeug
+        print("was started");
+
+
+        //starte Game.Restart()
+        //GameObject.FindObjectOfType<Game>().Restart();
+        game.Restart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
