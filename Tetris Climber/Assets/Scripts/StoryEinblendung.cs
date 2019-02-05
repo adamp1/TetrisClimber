eecc09091fd036ff8b84376f0ef9c72269cfe890
adamp1 <45352374+adamp1@users.noreply.github.com>
2019-02-05 13:25:51 +0100

    story progress läuft

--- Tetris Climber/Assets/Scripts/StoryEinblendung.cs
+++ Tetris Climber/Assets/Scripts/StoryEinblendung.cs
@@ -7,26 +7,52 @@ public class StoryEinblendung : MonoBehaviour
 {
 
     public Game game;
+    public Text uistory;
+    public string textstory;
+    public string currenttxt;
+    public float chartime = 0.1f;
+    public GameObject menu;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    public void StoryStart(){
+    IEnumerator StoryProgress()
+    {
+        currenttxt = "";
+        int i = 0;
+
+        while (currenttxt != textstory)
+        {
+            currenttxt += textstory[i];
+            i++;
+            uistory.text = currenttxt;
+            yield return new WaitForSeconds(chartime);
+        }
+    }
+
+    public void StoryStart()
+    {
         //ganz viel zeug
         print("was started");
+        uistory.gameObject.SetActive(true);
+        textstory = uistory.text;
+        uistory.text = "";
 
+        StartCoroutine(StoryProgress());
+
+        //deactivate main menu
 
         //starte Game.Restart()
         //GameObject.FindObjectOfType<Game>().Restart();
-        game.Restart();
+        //game.Restart();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
