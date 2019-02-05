db1713a99450183aba421db44da12bfcd56dc548
adamp1 <45352374+adamp1@users.noreply.github.com>
2019-02-05 15:47:18 +0100

    spawns objs

--- Tetris Climber/Assets/FallingObjects.cs
+++ Tetris Climber/Assets/FallingObjects.cs
@@ -6,18 +6,35 @@ public class FallingObjects : MonoBehaviour
 {
     public GameObject[] objects;
     public float min, max;
-    public float spawndistance = 50;
+    public float t;
+    public Vector2 spawndistance;
     public float speed = 5;
     public float deathdistance = 100;
+    GameObject a;
+    PlayerMovement spieler;
     // Start is called before the first frame update
     void Start()
     {
-        
+        t = Random.Range(min, max);
+        spieler = GameObject.FindObjectOfType<PlayerMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        t -= Time.deltaTime;
+
+        if (t <= 0)
+        {
+            int i = Random.Range(0, objects.Length-1);
+            a = Instantiate(objects[i],spieler.transform.position + new Vector3(0, spawndistance.x, spawndistance.y), 
+            objects[i].transform.rotation, transform) as GameObject;
+
+            //t = Random.Range(min, max);
+        }
+
+        if (a){
+            
+        }
     }
 }
