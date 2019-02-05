f1c18670882815c26aa7c61801618e35aa87cb5a
adamp1 <45352374+adamp1@users.noreply.github.com>
2019-02-05 15:55:39 +0100

    game objects können fallen

--- Tetris Climber/Assets/FallingObjects.cs
+++ Tetris Climber/Assets/FallingObjects.cs
@@ -10,7 +10,7 @@ public class FallingObjects : MonoBehaviour
     public Vector2 spawndistance;
     public float speed = 5;
     public float deathdistance = 100;
-    GameObject a;
+    public GameObject a;
     PlayerMovement spieler;
     // Start is called before the first frame update
     void Start()
@@ -24,7 +24,7 @@ public class FallingObjects : MonoBehaviour
     {
         t -= Time.deltaTime;
 
-        if (t <= 0)
+        if (t <= 0 && !a)
         {
             int i = Random.Range(0, objects.Length-1);
             a = Instantiate(objects[i],spieler.transform.position + new Vector3(0, spawndistance.x, spawndistance.y), 
@@ -34,7 +34,12 @@ public class FallingObjects : MonoBehaviour
         }
 
         if (a){
+            a.transform.position += Vector3.down * speed * Time.deltaTime;
 
+            if (Vector3.Distance(a.transform.position, spieler.transform.position) >= deathdistance){
+                Destroy(a);
+                a = null;
+            }
         }
     }
 }
