1e0ef77f5a6b6651f7da705b614e7e274c8df0a3
adamp1 <45352374+adamp1@users.noreply.github.com>
2019-02-12 11:38:49 +0100

    distance kill for faller

--- Tetris Climber/Assets/Prefabs/Fallende/Faller.cs
+++ Tetris Climber/Assets/Prefabs/Fallende/Faller.cs
@@ -6,12 +6,15 @@ public class Faller : MonoBehaviour
 {
     float speed = 10;
     float max = 10;
+    float distancemax = 100;
+    Vector3 startpos;
     // Start is called before the first frame update
     void Start()
     {
         transform.position += new Vector3(rand(), rand(), rand());
         speed = Random.Range(speed, speed * 2);
         print(speed);
+        startpos = transform.position;
     }
 
     float rand()
@@ -23,5 +26,10 @@ public class Faller : MonoBehaviour
     void Update()
     {
         transform.position += transform.up * -speed * Time.deltaTime;
+
+        if (Vector3.Distance(transform.position, startpos) > distancemax)
+        {
+            Destroy(gameObject);
+        }
     }
 }
